9dbce23b2a14374ba2d155948806240670859113
Nick Plagman <nrplagma@gmail.com>
2017-02-18 17:28:13 -0500

    Category stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,12 @@ class ProductsController < ApplicationController
     @user = current_user ||= ((User.find(session[:user_id]) if session[:user_id]) || User.new)
     @products = Product.search(params[:term])
 
+    if params[:category].blank?
+      @products = Product.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @products = Product.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
 
