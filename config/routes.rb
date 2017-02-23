46b83fca460fe49483b0fffeb002d8cc17c05eae
Nick Plagman <nrplagma@gmail.com>
2017-02-23 01:53:38 -0500

    Ready for affiliate

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'pages/FAQ'
 
+  get 'pages/picks'
+
   resources :categories
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
