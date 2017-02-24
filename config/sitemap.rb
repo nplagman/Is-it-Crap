46773c3af344db697e8264d2036cf4b9a545ad15
Nick Plagman <nrplagma@gmail.com>
2017-02-23 20:17:12 -0500

    Sitemap, vw

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,9 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://nickspicks.herokuapp.com"
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
+SitemapGenerator::Sitemap.sitemaps_host = "http://isitcrap.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
