7d75da187a87c56fd535bfb129c7765825fd9a71
Nick Plagman <nrplagma@gmail.com>
2017-07-23 09:59:34 -0400

    Removed script from gal analytics, plus 50 other files no idea

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
-<script>
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -25,4 +25,3 @@
   ga('create', 'UA-92830653-2', 'auto');
   ga('send', 'pageview');
 
-</script>
